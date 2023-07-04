b5590181b58845b4774145ca529f6660d36b6272
mallendeo <gh@mallendeo.com>
2023-07-04 22:52:38 +0000

    feat: :sparkles: add PWA module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,33 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/eslint-module',
+    '@vite-pwa/nuxt',
   ],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'QR Vault',
+      short_name: 'QR Vault',
+      icons: [
+        // { src: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+        // { src: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+      ],
+      theme_color: '#1F2937',
+      background_color: '#1F2937',
+      display: 'standalone',
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  },
 })
