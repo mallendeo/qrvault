a607c6bd6c94eb257c69ef3eb6e0cdec9110c586
mallendeo <gh@mallendeo.com>
2023-07-05 20:44:24 +0000

    feat(ui): add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup>
+import '~/assets/css/app.css'
+
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - QR Vault` : 'QR Vault'
+  },
+})
+</script>
+
+<template>
+  <slot />
+</template>
+
+<style>
+body {
+  --at-apply: "bg-white"
+}
+
+.dark-mode body {
+  --at-apply: "bg-dark"
+}
+</style>
