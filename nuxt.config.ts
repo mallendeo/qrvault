8d26881a67edb272fb86c082c99d0216ff12efb9
mallendeo <gh@mallendeo.com>
2023-07-05 20:41:36 +0000

    feat(ui): add @nuxt/color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/eslint-module',
+    '@nuxtjs/color-mode',
     '@vite-pwa/nuxt',
     'nuxt-icon',
     '@unocss/nuxt',
